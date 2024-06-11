1c18dfa0f6cab02a5d7717b3e04ff10a6173f3ef
niharika1102 <niharikag1102@gmail.com>
2024-06-11 15:59:30 +0530

    Initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl bg-blue-900 text-red-300 text-center p-5 m-5">
+      Hello mate
+    </h1>
+  )
+}
