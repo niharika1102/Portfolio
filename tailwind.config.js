2b534bf8e9893359a8d5263de38d56129ba26195
niharika1102 <niharikag1102@gmail.com>
2024-06-11 17:30:44 +0530

    Custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        'spartan': ['Spartan-MB-SemiBold', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#DC143C",
+        'secondary': "#880C25",
+        "slate-gray": "#AAAAAA",
+        "white-logo": "#FFFFFF"
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      screens: {
+        "wide": "1440px"
+      },
+    },
   },
   plugins: [],
 }
