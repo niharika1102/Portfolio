5abfcd17d99f1d7a12bdf606d0740e2bdc90ba14
niharika1102 <niharikag1102@gmail.com>
2024-06-27 00:37:33 +0530

    Projects section done

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,40 +1,26 @@
-// import React from 'react'
-
-// function Projects() {
-//   return (
-//     <div>Projects</div>
-//   )
-// }
-
-// export default Projects
-
-
-
 import React from 'react';
 import ProjectCard from '../components/ProjectCard';
+import { projects } from '../../constants';
+import Button from '../components/Button';
+import { NavLink } from 'react-router-dom';
 
 const Projects = () => {
-  const projectData = [
-    {
-      title: 'Chitkim Grocery Store',
-      description: 'A website for grocery shopping made using HTML and CSS.',
-    },
-    {
-      title: 'Netflix Clone',
-      description: 'The landing page of Nutsla cloned, built with HTML and CSS.',
-    },
-    // Add more project data as needed
-  ];
-
   return (
-    <div className="bg-black text-white">
-      <h2 className="text-2xl font-bold text-center pt-8">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-8">
-        {projectData.map((project, index) => (
-          <ProjectCard key={index} title={project.title} description={project.description} />
+    <section className="container py-12 px-10 sticky">
+      <h2 className="text-[30px] font-title-big font-bold text-lGreen mb-8">Projects</h2>
+      <div className="grid gap-8">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
         ))}
       </div>
-    </div>
+      <div className="flex justify-center mt-8">
+        <NavLink
+          to="https://github.com/niharika1102/"
+        >
+          <Button label="More Projects" />
+          </NavLink>
+      </div>
+    </section>
   );
 };
 
