7e5de8190f9989ca26bd3fa4984e46200edd10e9
niharika1102 <niharikag1102@gmail.com>
2024-06-27 00:12:35 +0530

    Fixed background

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -7,13 +7,13 @@ function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className='sticky padding-x py-8 absolute z-10 w-full bg-black-1'>
+    <header className='sticky padding-x py-8 z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
-        <NavLink to="/">
+        <a href="/">
           <p className='font-bold text-[36px] text-transparent bg-clip-text bg-gradient-1 font-title-big'>
             Niharika
           </p>
-        </NavLink>
+        </a>
 
         <div className='hidden lg:flex flex-1 justify-center lg:items-center lg:gap-16'>
           <ul className='flex flex-1 justify-center items-center gap-16'>
