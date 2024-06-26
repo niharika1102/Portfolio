a92251eff724952ee3114c685e5696a8053f268c
niharika1102 <niharikag1102@gmail.com>
2024-06-27 00:37:21 +0530

    Project card - commit 2

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,8 +4,8 @@ import { Github, Globe } from "lucide-react";
 
 const ProjectCard = ({ title, description, githubLink, demoLink, projectImage }) => {
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center bg-gradient-to-r from-darkblue-900 to-darkblue-700 p-6 rounded-xl shadow-lg">
-      <div className="relative w-full md:w-1/2 mt-6 md:mt-0">
+    <div className="flex flex-col md:flex-row items-center p-6 rounded-xl shadow-lg">
+      <div className="relative flex-shrink-0 w-full md:w-1/2 mt-6 md:mt-0">
         <img
           src={laptop}
           alt="Laptop Graphic"
@@ -15,14 +15,14 @@ const ProjectCard = ({ title, description, githubLink, demoLink, projectImage })
           <img
             src={projectImage}
             alt={title}
-            className="w-full h-full object-fill"
+            className="w-full h-full object-contain"
             style={{ clipPath: 'polygon(13% 17%, 87% 17%, 87% 83%, 13% 83%)' }}
           />
         </div>
       </div>
-      <div className="md:ml-8 text-white flex flex-col justify-center">
-        <h3 className="text-2xl font-bold">{title}</h3>
-        <p className="text-lg mb-4">{description}</p>
+      <div className="md:ml-8 w-full md:w-1/2 text-white flex flex-col justify-center">
+        <h3 className="text-[20px] font-body-1 pb-6">{title}</h3>
+        <p className="text-[20px] text-white-48 mb-4 pb-8">{description}</p>
         <div className="flex items-center space-x-4">
           {githubLink && (
             <a href={githubLink} target="_blank" rel="noopener noreferrer" className='transition duration-300 ease-in-out hover:bg-white-12 hover:rounded p-1'>
