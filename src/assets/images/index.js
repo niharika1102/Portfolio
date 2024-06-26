4a4ab9c3751c5b156780f9531f6ff0a6792f7b61
niharika1102 <niharikag1102@gmail.com>
2024-06-27 00:36:38 +0530

    Added images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,10 +2,16 @@ import profile from "./profile.svg"
 import laptop from "./laptop.svg"
 import mobile from "./mobile.svg"
 import chitkaraGroceryStore from "./chitkara-grocery-store.svg"
+import yelpCamp from './yelpcamp.png'
+import nike from "./nike.png"
+import movieland from "./movieland.png"
 
 export {
     profile,
     laptop,
     mobile,
-    chitkaraGroceryStore
+    chitkaraGroceryStore,
+    yelpCamp,
+    nike,
+    movieland
 }
