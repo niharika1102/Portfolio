186cf62696ee1847f304488a3f79dc47647e0fab
niharika1102 <niharikag1102@gmail.com>
2024-06-27 00:11:16 +0530

    Project Card component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,43 @@
 import React from "react";
+import { laptop } from "../assets/images";
+import { Github, Globe } from "lucide-react";
 
-const ProjectCard = ({ title, description }) => {
-    return (
-      <div className="bg-gray-900 p-6 rounded-lg shadow-md">
-        <div className="flex justify-center items-center space-x-4">
-          {/* Laptop Mockup */}
-          <div className="w-1/2 bg-gray-300 h-64 rounded-lg"></div>
-          {/* Mobile Mockup */}
-          <div className="w-1/4 bg-gray-300 h-96 rounded-lg"></div>
+const ProjectCard = ({ title, description, githubLink, demoLink, projectImage }) => {
+  return (
+    <div className="flex flex-col-reverse md:flex-row items-center bg-gradient-to-r from-darkblue-900 to-darkblue-700 p-6 rounded-xl shadow-lg">
+      <div className="relative w-full md:w-1/2 mt-6 md:mt-0">
+        <img
+          src={laptop}
+          alt="Laptop Graphic"
+          className="w-full"
+        />
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+          <img
+            src={projectImage}
+            alt={title}
+            className="w-full h-full object-fill"
+            style={{ clipPath: 'polygon(13% 17%, 87% 17%, 87% 83%, 13% 83%)' }}
+          />
         </div>
-        <h3 className="text-white text-xl mt-4">{title}</h3>
-        <p className="text-gray-400">{description}</p>
       </div>
-    );
+      <div className="md:ml-8 text-white flex flex-col justify-center">
+        <h3 className="text-2xl font-bold">{title}</h3>
+        <p className="text-lg mb-4">{description}</p>
+        <div className="flex items-center space-x-4">
+          {githubLink && (
+            <a href={githubLink} target="_blank" rel="noopener noreferrer" className='transition duration-300 ease-in-out hover:bg-white-12 hover:rounded p-1'>
+              <Github />
+            </a>
+          )}
+          {demoLink && (
+            <a href={demoLink} target="_blank" rel="noopener noreferrer" className='transition duration-300 ease-in-out hover:bg-white-12 hover:rounded p-1'>
+              <Globe />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
