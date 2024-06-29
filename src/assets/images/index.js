ef849e0205cc9768aa303f5dca6786d57feb125b
niharika1102 <niharikag1102@gmail.com>
2024-06-29 08:58:17 +0530

    Tools logos

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,6 +5,24 @@ import chitkaraGroceryStore from "./chitkara-grocery-store.svg"
 import yelpCamp from './yelpcamp.png'
 import nike from "./nike.png"
 import movieland from "./movieland.png"
+import mongodb from "./mongodb-svgrepo-com.svg"
+import express from "./express-svgrepo-com.svg"
+import aws from "./aws-svgrepo-com.svg"
+import css from './css-svgrepo-com.svg'
+import docker from './docker-svgrepo-com.svg'
+import figma from './figma-svgrepo-com.svg'
+import git from './git-svgrepo-com.svg'
+import github from './github-svgrepo-com.svg'
+import html from './html-5-svgrepo-com.svg'
+import java from './java-svgrepo-com.svg'
+import js from './js-svgrepo-com.svg'
+import mysql from './mysql-logo-svgrepo-com.svg'
+import node from './node-svgrepo-com.svg'
+import react from './react-svgrepo-com.svg'
+import tailwind from './tailwind-svgrepo-com.svg'
+import bootstrap from './bootstrap-fill-svgrepo-com.svg'
+import ubuntu from './ubuntu-svgrepo-com.svg'
+import postman from './postman-icon-svgrepo-com.svg'
 
 export {
     profile,
@@ -13,5 +31,23 @@ export {
     chitkaraGroceryStore,
     yelpCamp,
     nike,
-    movieland
+    movieland,
+    mongodb,
+    express,
+    aws,
+    css,
+    docker,
+    figma,
+    git,
+    github,
+    html,
+    java,
+    js,
+    mysql,
+    node,
+    react,
+    tailwind,
+    bootstrap,
+    postman,
+    ubuntu
 }
