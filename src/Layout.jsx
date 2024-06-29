5925e09c43773b6527749d5eb495332c341b4ec3
niharika1102 <niharikag1102@gmail.com>
2024-06-29 19:05:18 +0530

    Code cleanup

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,32 +3,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './sections/Header';
 import Footer from './sections/Footer';
-import OpeningSection from './sections/OpeningSection';
-import Projects from './sections/Projects';
-import ToolsAndTechnology from './sections/ToolsAndTechnology';
-import AboutMe from './sections/AboutMe';
 
 function Layout() {
   return (
     <main className='flex flex-col min-h-screen bg-black-1 bg-fixed'>
         <Header />
         <Outlet />
-        {/* <OpeningSection/> */}
-        {/*<Projects/>
-        <ToolsAndTechnology/>
-        <AboutMe/> */}
         <Footer/>
     </main>
-
-    // <main className="relative">
-    //   <Header/>
-    //   <section>
-    //     <Outlet/>
-    //   </section>
-    //   <section>
-    //     <Footer/>
-    //   </section>
-    // </main>
   );
 }
 
