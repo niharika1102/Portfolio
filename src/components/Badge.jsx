a6c0754db30be1a4214326b7935b51ebe657ec82
niharika1102 <niharikag1102@gmail.com>
2024-06-29 09:18:52 +0530

    Badge Component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Badge = ({text}) => {
+  return (
+    <span className="bg-transparent text-lGreen text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-lGreen dark:text-dGreen">{text}</span>
+  )
+}
+
+export default Badge
