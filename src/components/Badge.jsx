c8fb39b7559d26ad2752474e4b834368155a16b7
niharika1102 <niharikag1102@gmail.com>
2024-07-11 21:30:27 +0530

    Skill badge fixed

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Badge = ({text}) => {
   return (
-    <span className="bg-transparent text-lGreen text-sm font-medium me-2 px-2.5 py-0.5 mb-4 rounded dark:bg-lGreen dark:text-dGreen">{text}</span>
+    <span className="bg-transparent text-lGreen text-sm font-medium me-2 px-2.5 py-0.5 mb-4 rounded dark:bg-lGreen-20 dark:text-lGreen">{text}</span>
   )
 }
 
