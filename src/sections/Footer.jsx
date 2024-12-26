2afb628bbf80d5557bf1d84a442bc5a4ebbb6865
Niharika <niharikag1102@gmail.com>
2024-12-26 12:20:18 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <section className="bg-gradient-to-r from-dark-500 to-dark-900 p-6 md:p-8">
       <h2 className="text-lGreen text-xl md:text-3xl font-title-big font-bold mb-6 md:mb-8">Get In Touch</h2>
       <div className="space-y-4">
-        <div className="flex items-center space-x-2 md:space-x-4 text-white-1">
+{/*         <div className="flex items-center space-x-2 md:space-x-4 text-white-1">
           <img 
             src={phone} 
             alt="Phone"
@@ -15,7 +15,7 @@ const Footer = () => {
             height={32}
           />
           <span className="text-sm font-body-1 md:text-base">+91 94639 12634</span>
-        </div>
+        </div> */}
         <div className="flex items-center space-x-2 md:space-x-4 text-white-1">
           {/* <Mail className="w-5 h-5 md:w-6 md:h-6 text-lGreen" /> */}
           <img 
